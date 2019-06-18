2bdce82ab3a8e79e5ed44b71093cb45e10e90232
Farias <pablogarciafarias@gmail.com>
2019-06-18 17:21:02 -0300

    5

---
+++ controle/switch.js
@@ -0,0 +1,7 @@
+const imprimirResultado = function (nota) {
+    switch (Math.floor(nota)) {
+        case 10:    // se for 10 ou 9 ele executa as 2 linhas
+        case 9:
+            console.log('Quadro de Honra')
+    }
+}
