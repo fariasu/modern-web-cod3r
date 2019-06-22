2c0c115ba27bd8757edd92708751bf240ebf22f9
Farias <pablogarciafarias@gmail.com>
2019-06-22 13:23:25 -0300

    1

---
+++ array/reduce1.js
@@ -0,0 +1,15 @@
+const alunos = [ 
+    { nome: 'João', nota: 7.3, bolsista: false},
+    { nome: 'Maria', nota: 9.2, bolsista: true},
+    { nome: 'Pedro', nota: 9.8, bolsista: false},
+    { nome: 'Farias', nota: 8.7, bolsista: true}
+
+]
+
+console.log(alunos.map(a => a.nota))
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador,atual) {
+    console.log(acumulador, atual)
+    return acumulador + atual
+}/*, 10  <- (passar número inicial) */)
+
+console.log(resultado)
